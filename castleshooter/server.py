34e13a69f77c46f1c3fe0b2420bdd2a3974f8c02
Adam Yedidia <adamyedidia@gmail.com>
2022-11-21 21:36:47 -0500

    more server code

--- castleshooter/server.py
+++ castleshooter/server.py
@@ -1,6 +1,7 @@
 from settings import PORT, SERVER
 from socket import socket
 from typing import Any
+from redis_utils import rset, rget, redis_lock
 
 
 class Connection():
@@ -14,18 +15,32 @@ class Connection():
         return f"<Connection {self.id}: {self.addr}>"
 
 
-
 class Game():
-    def __init__(self):
-        self.num_clicks = 0
-    
-    def 
+    _CLICK_COUNTER_REDIS_KEY = 'click_counter'
 
+    def get_num_clicks(self) -> int:
+        return int(rget(_CLICK_COUNTER_REDIS_KEY)) or 0
+
+    def increment_num_clicks(self):
+        num_clicks = self.get_num_clicks()
+        with redis_lock('click_counter_lock'):
+            rset(_CLICK_COUNTER_REDIS_KEY, num_clicks + 1)
+
+    def handle_data_from_client(self, data: str):
+        if data == 'click':
+            self.increment_num_clicks()
+            
 
 def _get_new_connection_id(active_connections_by_id: dict[int, Connection]) -> int:
     if active_connections_by_id:
         return max(active_connections_by_id.keys()) + 1
     return 0
+
+
+def handle_connection(connection: Connection, game: Game) -> None:
+    while True:
+        data = conn.recv(4096).decode()
+        game.handle_data_from_client(data)
 
 
 def main() -> None:
@@ -38,8 +53,11 @@ def main() -> None:
     while True:
         conn, addr = s.accept()
         new_connection_id = _get_new_connection_id(active_connections_by_id)
-        active_connections_by_id[new_connection_id] = Connection(new_connection_id, conn, addr)
+        connection = Connection(new_connection_id, conn, addr)
+        active_connections_by_id[new_connection_id] = connection
         
+        gevent.spawn(handle_connection, connection, game)
+
         print(f'New connection: {connection}')
 
 
