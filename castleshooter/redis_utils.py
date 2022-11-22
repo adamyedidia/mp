122a792a1b0756db120965a5915b8b7c4a41370c
Adam Yedidia <adamyedidia@Adams-MacBook-Pro.local>
2022-11-21 23:45:29 -0500

    More redis and client code

--- castleshooter/redis_utils.py
+++ castleshooter/redis_utils.py
@@ -6,15 +6,22 @@ import redis_lock as rl
 
 redis = r.Redis(connection_pool=r.ConnectionPool(host='localhost', port=6379, db=0))
 
-def rset(key: str, value: Any) -> bool:
-    return redis.set(key, value)
+def _get_redis_key(key: str, client: bool = False) -> str:
+    prefix = 'client' if client else 'server'
+    return f'{client}:{key}'
 
-def rget(key: str) -> Optional[str]:
-    return val.decode() if (val := redis.get(key)) is not None else None
+def rset(key: str, value: Any, client: bool = False) -> bool:
+    return redis.set(_get_redis_key(key, client=client), value)
+
+def rget(key: str, client: bool = False) -> Optional[str]:
+    return val.decode() if (val := redis.get(_get_redis_key(key, client=client))) is not None else None
+
+def flushall() -> None:
+    redis.flushall()
 
 @contextmanager
-def redis_lock(key: str) -> Any:
-    lock = rl.Lock(redis, key)
+def redis_lock(key: str, client: bool = False) -> Any:
+    lock = rl.Lock(redis, _get_redis_key(key, client=client))
     lock.acquire()
     try:
         yield
