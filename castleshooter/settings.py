8ae5be2b23cb8871a8af73a490ae7f2a0fde2657
Adam Yedidia <adamyedidia@gmail.com>
2022-11-23 15:47:48 -0500

    local_settings and update readme

--- castleshooter/settings.py
+++ castleshooter/settings.py
@@ -2,5 +2,6 @@ import socket
 
 # TODO: make as input whether this is local or not
 SERVER = '192.168.0.21'
-# SERVER = socket.gethostbyname('localhost')
 PORT = 5555
+
+from local_settings import *
