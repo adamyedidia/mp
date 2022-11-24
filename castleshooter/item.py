3689cc1175522a5288412df4f2a9f7552a8dfc7a
lotta <l2gb28@gmail.com>
2022-11-24 01:39:34 -0500

    some more fleshing out items

--- castleshooter/item.py
+++ castleshooter/item.py
@@ -1,10 +1,21 @@
 import pygame
+from player import Player
+
 
 class Item():
-    def __init__(self, range: int):
+    def __init__(self, range: int = 0):
         self.range = range
 
-    def draw(self, x, y):
+    def draw(self, g: pygame.surface.Surface, x, y):
+        pass
+
+    def use(self, using_player: Player, effected_players: list[Player]):
+        pass
+
+    def hit_box(self):
+        # TODO: this function will return a function that decides
+        #  whether another player's coordinates x, y are within a hitbox
+        #  or something like that
         pass
 
 
@@ -15,3 +26,8 @@ class Sword(Item):
     def draw(self, g: pygame.surface.Surface, x: int, y: int):
         pygame.draw.rect(g, (0,0,0), (x+30, y+30, 15, 5))
         pygame.draw.rect(g, (0,0,0), (x+35, y+15, 5, 27))
+
+    def use(self, using_player: Player, effected_players: list[Player]):
+        using_player.item = Item()
+        for damaged_player in effected_players:
+            damaged_player.healthbar.damage()
