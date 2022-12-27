bf790b654e2af4a4e6781726824044392e002371
Adam Yedidia <adamyedidia@gmail.com>
2022-12-27 13:06:20 -0800

    Fix

--- castleshooter/server.py
+++ castleshooter/server.py
@@ -155,7 +155,7 @@ class GameState:
                         store_command(Command(1, CommandType.SPAWN, time=datetime.now(), client_id=client_id, 
                                     data={'x_pos': random.randint(1, GAME_WIDTH), 
                                             'y_pos': random.randint(1, GAME_HEIGHT),
-                                            'team': client_id_to_team[client_id].value}), for_client=client_id, client_id=None)
+                                            'team': client_id_to_team[client_id].value}), for_client=client_id, client_id=None, game_name=game_name)
 
                     rset('client_id_to_player_number', json.dumps(client_id_to_player_number), client_id=None, game_name=game_name)
                     rset('game_started', '1', client_id=None, game_name=game_name)
