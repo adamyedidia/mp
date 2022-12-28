8e24ba92b25ac47b8eb8d125b27c1109d08b6ef1
Adam Yedidia <adamyedidia@gmail.com>
2022-12-27 17:03:40 -0800

    Change spawn time

--- castleshooter/server.py
+++ castleshooter/server.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import zlib
 from team import get_team_for_client_id
@@ -164,7 +164,7 @@ class GameState:
                     rset('game_names', json.dumps(all_game_names), client_id=None, game_name=SPECIAL_LOBBY_MANAGER_GAME_NAME)
 
                     for i, client_id in enumerate(client_ids_in_game):
-                        store_command(Command(1, CommandType.SPAWN, time=datetime.now(), client_id=client_id, 
+                        store_command(Command(1, CommandType.SPAWN, time=datetime.now() - timedelta(seconds=5), client_id=client_id, 
                                     data={'x': random.randint(1, GAME_WIDTH), 
                                             'y': random.randint(1, GAME_HEIGHT),
                                             'team': client_id_to_team[client_id].value}), for_client=client_id, client_id=None, game_name=game_name)
